d3d970ba48b0161828a94fa997b73d9a08e3f490
mohamed atef <smsm85579@gmail.com>
2025-09-01 21:53:39 +0300

    create on_boarding item and on_boarding_item_model

---
+++ lib/features/on_boarding/data/on_boarding_item_model.dart
@@ -0,0 +1,11 @@
+class OnBoardingItemModel {
+  final String image;
+  final String title;
+  final String subtitle;
+
+  OnBoardingItemModel({
+    required this.image,
+    required this.title,
+    required this.subtitle,
+  });
+}
